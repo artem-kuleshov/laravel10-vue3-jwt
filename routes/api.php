792417c11e692a8d96a4e0aeba2ed8b1a8fdc7ed
Kuleshov Artem <artemkul57@mail.ru>
2023-11-12 17:49:53 +0300

    install tymon/jwt-auth + config backend

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'App\Http\Controllers', 'middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 });
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::group(['namespace' => 'App\Http\Controllers\User', 'prefix' => 'users'], function () {
     Route::post('/', 'StoreController');
